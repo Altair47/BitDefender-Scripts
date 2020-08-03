9b73e54e7e30ced5a52ecab9e6d7d034a550a1c5
Nick <Devilmanaltair1995@yahoo.com>
2020-08-03 14:58:31 +0300

    Catch exception, and .bat for ease
    Also replacements for some common google translation fallacies. Should add a list for it sometimes in the future.

--- BDTrans/Main.py
+++ BDTrans/Main.py
@@ -3,6 +3,7 @@ import time
 from bs4 import BeautifulSoup
 from config import *
 from googletrans import Translator
+import sys
 #Engage Driver
 driver = webdriver.Chrome()
 #Translator
@@ -10,7 +11,7 @@ translator = Translator()
 
 #Logging in
 time.sleep(1)
-fpage=input("Input site to start: ")
+fpage=input("Input entry to start: ")
 driver.get(fpage)
 print(driver.current_url)
 print(driver.title)
@@ -22,17 +23,31 @@ driver.find_element_by_xpath('//*[@id="login-form"]/form/div[3]/button').click()
 #Items to ignore: <#ProductName#>,[Product],
 
 while 1:
-	txttr=driver.find_element_by_id('id_rawsource').get_property("value");
-	print('+Source: ',txttr)
+	try:
+		txttr=driver.find_element_by_id('id_rawsource').get_property("value");
+		print('+Source: ',txttr)
+	
+		trns=translator.translate(txttr, dest='el')
+		trnsf=trns.text
+	
+		
+		driver.find_element_by_id('id_translation').clear()
+		trnsf=trnsf.replace('[Προϊόν]','​[Product]​')
+		trnsf=trnsf.replace('[Κεντρική]','​[Central]​')
+		trnsf=trnsf.replace('<a> <b> <c /> </b> </a>','​​<a>​ ​<b>​ ​<c/>​ ​</b>​ ​</a>')
+		trnsf=trnsf.replace('<b> <c> <d /> </c> </b>','​​<b> <c> <d /> </c> </b>')
+		trnsf=trnsf.replace('{ημερομηνία}','​​{date}')
+		trnsf=trnsf.replace('\"{info}\"','​​\'​{info}\'')
+		trnsf=trnsf.replace('{{factory_name}}','​​{{manufacturer_​name}}')
+		print('=Translated: ',trnsf)
 
-	trns=translator.translate(txttr, dest='el')
-	trnsf=trns.text
-
-	print('=Translated: ',trnsf)
-	driver.find_element_by_id('id_translation').clear()
-	driver.find_element_by_id('id_translation').send_keys(trnsf)
-
-	x=input('Press any key continue. Type N/n to stop.')
-	if x=='N' or x=='n': break
-
-	driver.find_element_by_xpath('//*[@id="pagecontent"]/div[1]/div/form/div[2]/button[1]').click()
+		driver.find_element_by_id('id_translation').send_keys(trnsf)
+	
+		x=input('Press any key continue. Type N/n to stop.')
+		if x=='N' or x=='n': break
+	
+		driver.find_element_by_xpath('//*[@id="pagecontent"]/div[1]/div/form/div[2]/button[1]').click()
+	except:
+		print("End of the line.\n", sys.exc_info()[0], "occurred.")
+		fpage=input("Input another entry to start: ")
+		driver.get(fpage)
